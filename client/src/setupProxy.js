09a8f0e62c123c0a142b17e3bfa9887588eb68f4
Israel <israel.o@itthynk.co.za>
2019-04-17 14:34:57 +0200

    adding aws http to the proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
-    app.use(proxy('/api/email', { target: 'http://localhost:8880/'}))
+    app.use(proxy('/api/email', { target: 'http://ec2-3-85-29-183.compute-1.amazonaws.com/'}))
 }
-
-//'http://ec2-3-85-29-183.compute-1.amazonaws.com/'
